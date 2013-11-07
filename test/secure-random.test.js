3f1cff8b41720161ab64395f295639f0a8ab9b5a
JP Richardson <jprichardson@gmail.com>
2013-11-07 08:43:06 -0600

    initial implementation complete

--- test/secure-random.test.js
+++ test/secure-random.test.js
@@ -1,1 +1,21 @@
+var secureRandom = require('../lib/secure-random')
 
+require('terst')
+
+describe('+ secureRandom(count)', function() {
+  describe('> when no options are passed', function() {
+    it('should generate a native type array of bytes', function() {
+      var bytes = secureRandom(10)
+      EQ (bytes.length, 10)
+      T (bytes instanceof Uint8Array)
+    })
+  })
+
+  describe('> when options.array is set', function() {
+    it('should generate a regular array', function() {
+      var bytes = secureRandom(10, {array: true})
+      EQ (bytes.length, 10)
+      T (bytes instanceof Array)
+    })
+  })
+})
